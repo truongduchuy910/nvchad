25cc6ab8f72c109b12a36ccd73d0794de58d551d
Trần Ngọc Huy <truongduchuy910@gmail.com>
2024-07-30 17:32:02 +0700

    obsidian

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,6 +1,7 @@
 require "nvchad.mappings"
 
 -- add yours here
+-- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvchad/mappings.lua
 
 local set = vim.keymap.set
 
@@ -9,6 +10,8 @@ set("i", "jk", "<ESC>")
 
 set({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
-set({ "n", "x", "o" }, "n", "<Plug>(leap-forward)")
-set({ "n", "x", "o" }, "N", "<Plug>(leap-backward)")
-set({ "n", "x", "o" }, "gn", "<Plug>(leap-from-window)")
+set({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
+set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
+set({ "n", "x", "o" }, "gs", "<Plug>(leap-from-window)")
+
+set("n", "<leader>fn", "<cmd>ObsidianSearch<CR>", { desc = "telescope Obsidian" })
